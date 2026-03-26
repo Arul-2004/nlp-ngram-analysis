352aa45339096e0ef0529619c5d46337ee6c0308
Arularasi S <arularasisivakumar20@gmail.com>
2026-03-26 11:20:10 +0530

    Add files via upload
    This project demonstrates how to build an n-gram language model from scratch in Python. It processes input text, generates n-grams (unigrams, bigrams, trigrams), and calculates their frequencies and probabilities.
    Unlike library-based approaches, this implementation manually performs tokenization and n-gram creation, making it ideal for learning core NLP concepts.

---
+++ ngram.py
@@ -0,0 +1,44 @@
+text = "Robots are smart. Robots work quickly."
+n = 3
+
+def tokenize(text):
+    text = text.lower()
+    for p in ".,!?;:":
+        text = text.replace(p, " ")
+    return text.split()
+
+tokens = tokenize(text)
+
+def generate_ngrams(tokens, n):
+    ngrams = []
+    for i in range(len(tokens) - n + 1):
+        ngrams.append(tuple(tokens[i:i+n]))
+    return ngrams
+
+ngram_list = generate_ngrams(tokens, n)
+
+ngram_freq = {}
+for ng in ngram_list:
+    if ng in ngram_freq:
+        ngram_freq[ng] += 1
+    else:
+        ngram_freq[ng] = 1
+
+total_ngrams = sum(ngram_freq.values())
+
+ngram_prob = {}
+for ng, count in ngram_freq.items():
+    ngram_prob[ng] = count / total_ngrams
+
+print("\nOriginal text:")
+print(text)
+
+print(f"\nTokens:\n{tokens}")
+
+print(f"\n{n}-grams and their frequency:")
+for k, v in ngram_freq.items():
+    print(k, ":", v)
+
+print(f"\n{n}-gram probabilities:")
+for k, v in ngram_prob.items():
+    print(k, ":", round(v, 3))
